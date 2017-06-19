3e4ef72474b1dd13171e53c3b890a5d7add8cb0a
Jeremy <jersoc@gmail.com>
2017-06-19 19:45:30 +0000

    add mlab

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://jersoc:jerwar11@ds133192.mlab.com:33192/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true }));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
